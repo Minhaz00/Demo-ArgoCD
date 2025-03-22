eb02a69c18d518655a8416055acdf27857f7f091
Minhaz Jisun <minhaz.jisun@gmail.com>
2025-03-23 03:20:20 +0600

    Remove node_modules from Git tracking

--- App/index.js
+++ App/index.js
@@ -1,11 +1,20 @@
-const express = require('express');
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello from MyApp! Version 1.0.0' });
+  res.send("Hello from MyApp! Version 1.0.0");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.get('/health', (req, res) => {
+  res.send('OK');
 });
+
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = { app, server };
