6fa7eec41ad88dd55e6ea04f5399b883a26ad138
Minhaz Jisun <minhaz.jisun@gmail.com>
2025-03-24 03:02:08 +0600

    minor updates

--- App/index.js
+++ App/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 // Root route
 app.get('/', (req, res) => {
-  res.send("Hello from MyApp! App version 1.0");
+  res.send("Hello from MyApp! App version 1.1");
 });
 
 // Health check route
